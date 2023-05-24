feae34432983c3290aed21ccfc3a4e3e08b54f67
Shudhanshu Jaiswal <shudhanshu.jaiswal@freecharge.com>
2023-05-24 05:50:29 +0530

    okta implementation

--- src/main/java/com/dailyCodeBuffer/productService/controller/ProductController.java
+++ src/main/java/com/dailyCodeBuffer/productService/controller/ProductController.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,11 +19,13 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @PreAuthorize("hasAuthority('Admin')")
     @PostMapping
     public ResponseEntity<Long> addProduct(@RequestBody ProductRequest productRequest ){
         long productId = productService.addProduct(productRequest);
         return new ResponseEntity<>(productId, HttpStatus.CREATED);
     }
+
 
     @GetMapping
     public ResponseEntity<List<ProductResponse>>getProduct(){
@@ -30,6 +33,7 @@ public class ProductController {
         return new ResponseEntity<>(list,HttpStatus.OK);
     }
 
+    @PreAuthorize("hasAuthority('Admin')||hasAuthority('Customer')||hasAuthority('SCOPE_internal')")
     @GetMapping("/{id}")
     public ResponseEntity<ProductResponse>getProductById(@PathVariable("id") long productId){
         ProductResponse productResponse = productService.getProductById(productId);
